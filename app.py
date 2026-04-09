645fe13985c80a672411ba0d788ef6bb0f4e515a
Pranav Chandrakant Patil <pranavp1560@gmail.com>
2026-04-09 10:50:59 +0530

    COMMIT ALL FILES

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+app.secret_key = "secretkey"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        phone = request.form.get("phone")
+        message = request.form.get("message")
+
+        print(name, phone, message)  # later store in DB
+
+        flash("Your request submitted successfully!")
+        return redirect(url_for("contact"))
+
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
