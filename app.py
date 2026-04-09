81234ecfbaadace4a4270df0a2ca509a457e54ca
Pranav Chandrakant Patil <pranavp1560@gmail.com>
2026-04-09 10:54:34 +0530

    Add environment variable support for app host and port

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+
 
 app = Flask(__name__)
 app.secret_key = "secretkey"
@@ -33,5 +35,6 @@ def contact():
 
     return render_template("contact.html")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
